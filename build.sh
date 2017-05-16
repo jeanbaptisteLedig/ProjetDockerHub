d2170de1b212ee424c004e1e0bf6998ea9138503
RobinDudek <robin.dudek@ynov.com>
2017-05-16 17:25:28 +0200

    test 2

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 INSTANCE_RUNNING=`sudo docker ps --filter="ancestor=jeanbaptisteledig/ProjetDockerHub" | wc -l`
 
 # Build docker image
-docker build -t jeanbaptisteledig/projetdockerhub .
+docker build -t aimad/projetdockerhub .
 
 # Stop current instance if exists
 if [ $INSTANCE_RUNNING -gt 1 ] ; then
-    printf "Stopping jeanbaptisteLedig/ProjetDockerHub"
+    printf "Stopping jaimad/ProjetDockerHub"
     docker stop prod-devops
     docker rm prod-devops
 fi
 
-docker run --name prod-devops -p 80:80 -d jeanbaptisteledig/projetdockerhub
+docker run --name prod-devops -p 80:80 -d aimad/projetdockerhub
